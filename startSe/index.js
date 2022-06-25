0984c034a33bcdfabf3a3a301e4f20febd4169e3
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2022-06-25 08:44:42 -0300

    learning

--- startSe/index.js
+++ startSe/index.js
@@ -1,3 +1,15 @@
-var nome = prompt('Qual seu nome?')
+// var nome = prompt('Qual seu nome?')
 
-document.querySelector("#tittle").textContent = `Seja bem vindo, ${nome}!`;
+// document.querySelector("#tittle").textContent = `Seja bem vindo, ${nome}!`;
+
+
+
+// function mudaCorFundo(){
+
+//     const menu = document.querySelector('#coresFundo');
+//     const cor = menu.options[menu.selectedIndex].value;
+//     document.body.style.background = cor;
+// }
+
+
+// Array = [pop, splice, unshift, shift]
